bcae426e0a68484dc5922162138124db987e9bbc
$RANDOM_NAME <pay@go.pekarius>
2025-04-28 13:48:45 +0300

    update

--- obs-tools.spec
+++ obs-tools.spec
@@ -1,5 +1,5 @@
 Name: obs-tools
-Version: 23
+Version: 24
 Release: 0
 License: LGPL
 Summary: %{name}
--- pkg_check_available.sh
+++ pkg_check_available.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-typeset -i "iterations=$2"
-typeset -i "timeout=$3"
-typeset -a variables=()
-
-while IFS= read -r line
-do
-    variables+=("$line")
-done <<< "$1"
+typeset -i "iterations=$1"
+typeset -i "timeout=$2"
+shift
+shift
 
 for ((i=1; i<=iterations; i++))
 do
-    dnf provides "${variables[@]}" && break
+    dnf provides "${@}" && break
     sleep "${timeout}"
 done
+
